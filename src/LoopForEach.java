9f846c08825635f1b77dbe589329dbb705fb8fe7
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-11-01 13:26:36 -0700

    message

---
+++ src/LoopForEach.java
@@ -0,0 +1,9 @@
+public class LoopForEach {
+
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "Mazda", "BMW", "Ford"};
+        for (String i : cars){
+            System.out.println("Cars: "+i);
+        }
+}
+}
