0f3e9658f28d802afefc00a739b55edcf42542be
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-11-01 13:56:42 -0700

    message

---
+++ src/MultiDimentionalArray.java
@@ -0,0 +1,10 @@
+import javax.swing.text.Style;
+
+public class MultiDimentionalArray {
+
+    public static void main(String[] args) {
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+         // Number of Rows in the array.
+         System.out.println(myNumbers[0][1]);
+    }
+}
