87ad77d75bfca66dc2404a611c0a4ec22eb525a4
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2023-11-14 12:57:28 -0800

    message

---
+++ src/MethodParam.java
@@ -0,0 +1,9 @@
+public class MethodParam {
+    static void myMethod(String fname, int age){
+        System.out.println("Hello, "+fname+"! You are "+age);
+    }
+    public static void main(String[] args){
+        myMethod("best", 7);
+    }
+
+}
