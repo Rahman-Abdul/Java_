0c840f663af3e168c2e0a2b4830816e9fcfba2f9
Abdulrahman Ahmed <abdulrahmanahmedrasheed@gmail.com>
2024-09-03 17:30:28 -0700

    new commit

--- src/MyMethod.java
+++ src/MyMethod.java
@@ -2,7 +2,11 @@ public class MyMethod {
     static void mainMethod (){
         System.out.println("I just got a method!");
      }
+     static void secondMethod(){
+        System.out.println(6);
+     }
     public static void main(String[] args) {
+        secondMethod();
        mainMethod();
     }
 }
